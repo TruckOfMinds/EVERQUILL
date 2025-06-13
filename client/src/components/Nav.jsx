6d533ac52a5d865e0a806e0ff8424db3055632eb
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-06-13 14:16:41 +0100

    style: styled navbar except for pseudo classes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,22 +2,39 @@ import { Link } from "react-router";
 import Book from "./Book";
 import Quill from "./Quill";
 import "./styles/Nav.css";
+import LogoPlain from "./LogoPlain";
+import Paint from "./Paint";
 
 export default function Nav() {
   return (
-    <nav className="nav flex items-center justify-between">
-      <img src="/LogoAlt.svg" alt="EverQuill Logo" className="baumans" />
-      <span>
-        <span>
-          <Quill />
-          <Link to={"/"}>Write</Link>
+    <nav className="nav grid">
+      <Link to={"/"}>
+        <span className="flex items-center justify-between gap-[1rem]">
+          <LogoPlain />
+          <h1 className="text-inherit cagliostro text-[1.5rem]">EverQuill</h1>
         </span>
-        <span>
-          <Book />
-          <Link to={"/entries"}>View Entries</Link>
-        </span>
+      </Link>
+
+      <span className="flex items-center justify-between gap-[2rem]">
+        <Link to={"/"}>
+          <span className="nav-item ">
+            <Quill />
+            <h3 className="cagliostro">Write</h3>
+          </span>
+        </Link>
+
+        <Link to={"/entries"}>
+          <span className="nav-item">
+            <Book />
+            <h3 className="cagliostro">View Entries</h3>
+          </span>
+        </Link>
       </span>
-      <div>Change Theme</div>
+
+      <span className="nav-item theme rounded-full">
+        <Paint />
+        <h3 className="cagliostro">Change Theme</h3>
+      </span>
     </nav>
   );
 }
