1cd4a8c4f2aadf2dba46bec99b4528adfed11ee7
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-06-15 00:08:35 +0100

    feature: added form insertion chore: added prop error-remover

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -2,38 +2,90 @@ import { useEffect, useState } from "react";
 import "./styles/Write.css";
 
 export default function Write() {
+  const defFormValues = {
+    name: "",
+    tag: null,
+    msg: "",
+    date: getDate(),
+  };
   const [tags, setTags] = useState([]);
+  const [entry, setEntry] = useState(defFormValues);
 
   useEffect(() => {
+    /**
+     * @async @function pollForTags
+     * @description sets an interval to fetch the tags from the server,
+     * before setting the state of {array} tags to the response.
+     * Intervals are 1s apart.
+     */
     const pollForTags = setInterval(async () => {
-      const response = await fetch("http://localhost:8080/tags");
-      setTags(response);
+      try {
+        await fetch("http://localhost:8080/tags")
+          .then((response) => response.json())
+          .then((json) => setTags(json));
+      } catch (err) {
+        throw new Error(`Failed to connect to server [Error — ${err}]`);
+      }
     }, 1000);
-    clearInterval(pollForTags);
+
+    // returns clearing to prevent overlap — also makes it repeat idk y
+    return () => clearInterval(pollForTags);
   }, []);
 
+  /**
+   * @function getDate
+   * @returns {string} DMY formatted date string from a Date object.
+   */
   function getDate() {
     const timestamp = new Date();
     const day =
+      // adds leading 0
       timestamp.getDate() < 10
         ? `0${timestamp.getDate()}`
         : timestamp.getDate();
+
     const month =
+      // adds leading 0, adds 1 to correct month
       timestamp.getMonth() + 1 < 10
         ? `0${timestamp.getMonth() + 1}`
         : timestamp.getMonth() + 1;
+
     const year = timestamp.getFullYear();
 
     return `${day}/${month}/${year}`;
   }
 
+  function updateState(e) {
+    setEntry({ ...entry, [e.target.name]: e.target.value });
+  }
+
+  function sendUserData(e) {
+    e.preventDefault();
+    try {
+      fetch("http://localhost:8080/write", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(entry),
+      });
+      console.log(entry);
+    } catch (err) {
+      throw new Error(`Failed to send data [Error — ${err}`);
+    }
+  }
+
+  // header and form
   return (
     <main className="pt-[3rem] pb-[2rem] flex flex-col items-center justify-start h-3/4">
       <h1 className=" cagliostro text-[2rem] pb-[2rem]">
         Write a Message and be Remembered Forever!
       </h1>
 
-      <form className="form p-[2rem] min-w-fit w-3/4 h-full">
+      <form
+        className="form p-[2rem] min-w-fit w-3/4 h-full"
+        onSubmit={sendUserData}
+      >
         <label htmlFor="name" className="cagliostro name label">
           Name:
           <input
@@ -43,6 +95,8 @@ export default function Write() {
             className="baumans input"
             required
             aria-required
+            value={entry.name}
+            onChange={updateState}
           />
         </label>
 
@@ -53,11 +107,12 @@ export default function Write() {
             id="tag"
             defaultValue={"def"}
             className="baumans input"
+            onChange={updateState}
           >
-            <option value="def" className="baumans" hidden aria-hidden></option>
+            <option value="def" className="baumans" hidden></option>
 
             {tags.map((t) => (
-              <option key={t.id} value={t.tag_name} className="baumans">
+              <option key={t.id} value={t.id} className="baumans">
                 {t.tag_name}
               </option>
             ))}
@@ -73,6 +128,8 @@ export default function Write() {
             className="baumans input"
             required
             aria-required
+            value={entry.msg}
+            onChange={updateState}
           ></textarea>
         </label>
 
@@ -82,10 +139,11 @@ export default function Write() {
             type="text"
             name="date"
             id="date"
-            value={getDate()}
             className="baumans input"
             readOnly
             aria-readonly
+            value={entry.date}
+            onChange={updateState}
           />
         </label>
 
