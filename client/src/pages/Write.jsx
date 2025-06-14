c6b7c16a9da9ee7215dcbac9556165397515e28e
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-06-14 22:28:33 +0100

    style: nav and 90% of form

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -28,30 +28,33 @@ export default function Write() {
   }
 
   return (
-    <main className="pt-[2rem] pb-[2rem] flex flex-col items-center justify-start h-3/4">
+    <main className="pt-[3rem] pb-[2rem] flex flex-col items-center justify-start h-3/4">
       <h1 className=" cagliostro text-[2rem] pb-[2rem]">
         Write a Message and be Remembered Forever!
       </h1>
 
-      <form className="form p-[2rem] min-w-3/4 h-full">
-        <label htmlFor="name" className="cagliostro">
+      <form className="form p-[2rem] min-w-fit w-3/4 h-full">
+        <label htmlFor="name" className="cagliostro name label">
           Name:
           <input
             type="text"
             name="name"
             id="name"
-            className="baumans"
+            className="baumans input"
             required
             aria-required
           />
         </label>
 
-        <label htmlFor="tag" className="cagliostro">
+        <label htmlFor="tag" className="cagliostro tag label">
           Category:
-          <select name="tag" id="tag" defaultValue={"def"} className="baumans">
-            <option value="def" className="baumans">
-              Category
-            </option>
+          <select
+            name="tag"
+            id="tag"
+            defaultValue={"def"}
+            className="baumans input"
+          >
+            <option value="def" className="baumans" hidden aria-hidden></option>
 
             {tags.map((t) => (
               <option key={t.id} value={t.tag_name} className="baumans">
@@ -61,32 +64,32 @@ export default function Write() {
           </select>
         </label>
 
-        <label htmlFor="msg" className="cagliostro">
+        <label htmlFor="msg" className="cagliostro msg label">
           Your Message:
           <textarea
             type="text"
             name="msg"
             id="msg"
-            className="baumans"
+            className="baumans input"
             required
             aria-required
           ></textarea>
         </label>
 
-        <label htmlFor="date" className="cagliostro">
+        <label htmlFor="date" className="cagliostro date label">
           Date:
           <input
             type="text"
             name="date"
             id="date"
             value={getDate()}
-            className="baumans"
+            className="baumans input"
             readOnly
             aria-readonly
           />
         </label>
 
-        <button type="submit" className="cagliostro">
+        <button type="submit" className="cagliostro submit">
           Write
         </button>
       </form>
